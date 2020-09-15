ea41fd8dbcb72badad21965e78eaf43e9bf20fbf
Jagat Nandan Prasad <62509653+jagatabhay@users.noreply.github.com>
2020-09-15 23:27:25 +0530

    Add files via upload

--- test_session8.py
+++ test_session8.py
@@ -32,27 +32,14 @@ def test_readme_words_counts():
     assert len(readme_words) >= 100 , "Kindly define README properly"
 
 
-#4 Readme Proper Description
-def test_readme_proper_desscription():
-    READMELOOKSGOOD = True
-    readme = open('README.md','r')
-    readme_words = readme.read().split()
-    readme.close()
-    for words in README_CONTENT_CHECK_FOR:
-        if words not in readme_words:
-            READMELOOKSGOOD = False
-            pass
-    assert READMELOOKSGOOD == True , "You have not defined all functions/classes in README.md"
-
-
-#5 function name had caps letter
+#4 function name had caps letter
 def test_funcation_had_cap_letter():
     functions = inspect.getmembers(session8, inspect.isfunction )
     for function in functions:
         assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
 
 
-#6 PEP8 Guidelines
+#5 PEP8 Guidelines
 def test_fourspace():
     lines = inspect.getsource(session8)
     spaces = re.findall('\n +.', lines)
@@ -61,13 +48,13 @@ def test_fourspace():
         assert len(re.sub(r'[a-zA-Z#@\n\"\']', '', space)) % 4 == 0,"Your code intentation does not follow PEP8 guidelines" 
 
 
-#7 check docstring exist
+#6 check docstring exist
 def test_docstring_length():
     a = session8.check_fn_docs()
     assert a(session8.mul) == True
 
 
-#8 check next fibonacci number
+#7 check next fibonacci number
 def test_fibonacci():
     k = session8.next_fibo_number()
     k()
@@ -76,7 +63,7 @@ def test_fibonacci():
     assert k() == 2
 
 
-#9
+#8
 def test_function_single_dict_call():
     c = session8.function_counter( session8.add )
     c()
@@ -91,7 +78,7 @@ def test_function_single_dict_call():
 
 
 
-#10
+#9
 def test_function_calling_sep_dict():
     c = session8.function_two_dict(session8.add, session8.userA)
     c()
